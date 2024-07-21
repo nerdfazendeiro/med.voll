1f524b7da3df845f5ecea0ac055481d220aacaff
Rafael Ramalho Silveira <rafaelramalhosilveira@gmail.com>
2024-07-21 16:52:59 -0400

    Criando a classe, o controller e a tabela Paciente, logo depois criando o metodo POST(cadastrar)

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -8,6 +8,7 @@ import med.voll.api.endereco.Endereco;
 @Entity(name = "Medico")
 //Usando o Lombok, para criar o construtor, getters e setters, EqualsAndHashCode
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
@@ -37,62 +38,6 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCrm() {
-        return crm;
-    }
-
-    public void setCrm(String crm) {
-        this.crm = crm;
-    }
-
-    public Especialidade getEspecialidade() {
-        return especialidade;
-    }
-
-    public void setEspecialidade(Especialidade especialidade) {
-        this.especialidade = especialidade;
     }
 
     public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,35 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import med.voll.api.endereco.Endereco;
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter // O Lombok vai cuidar de Criar os Getters e Setters e o Construtor
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dados){
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.cpf = dados.cpf();
+        this.telefone = dados.telefone();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
